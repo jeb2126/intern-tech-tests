70d6141ca2e3d68e97a2d151f786eaa74453da6b
james barnes <jeb2126@gmail.com>
2022-04-07 15:48:04 +0100

    installed react-router-dom

--- react-tech-test/src/Solution/YourSolution.js
+++ react-tech-test/src/Solution/YourSolution.js
@@ -1,26 +1,51 @@
-import '../AdditionalFiles/App.css';
+import "../AdditionalFiles/App.css";
 import * as React from "react";
+import { useState, useEffect } from "react";
+import { mockFetch } from "../AdditionalFiles/mockFetch";
 
 //This is the API url to fetch from
-const API_URL = 'https://matchesfashion.com/api/products';
+const API_URL = "https://matchesfashion.com/api/products";
 const TAX_RATE = 0.08;
 
 function YourSolution() {
-    return (
+  const [products, setProducts] = useState([]);
+
+  // Fetch the product data
+  useEffect(() => {
+    mockFetch().then((productsFromApi) => {
+      setProducts(productsFromApi);
+    });
+  }, []);
+
+  console.log(products, "<<<<");
+
+  return (
     <div className="App">
       <table id="products">
         <thead>
-        <tr>
-          <th>Id</th>
-          <th>Brand</th>
-          <th>Name</th>
-          <th>Quantity Sold</th>
-          <th>Sold Price</th>
-          <th>Cost To Business</th>
-        </tr>
+          <tr>
+            <th>Id</th>
+            <th>Brand</th>
+            <th>Name</th>
+            <th>Quantity Sold</th>
+            <th>Sold Price</th>
+            <th>Cost To Business</th>
+          </tr>
         </thead>
-          <tbody>
-          </tbody>
+        <tbody>
+          {products.map((product) => {
+            return (
+              <tr>
+                <td>{product.id}</td>
+                <td>{product.brand}</td>
+                <td>{product.name}</td>
+                <td>{product.quantitySold}</td>
+                <td>{product.soldPrice}</td>
+                <td>{product.costToBusiness}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
       <button>First Page</button>
       <button>Previous Page</button>
